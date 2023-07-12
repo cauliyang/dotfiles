c05c848fa8bd6aae414cbcb7fbc16ad66564ef13
yangyangli <yangyang.li@northwestern.edu>
2023-07-11 19:15:22 -0500

    Update .config/lvim/lua/user/dap.lua Update .config/lvim/lua/user/lsp/languages/python.lua Update .config/lvim/lua/user/overseer.lua

--- dot_config/lvim/lua/user/dap.lua
+++ dot_config/lvim/lua/user/dap.lua
@@ -15,6 +15,60 @@ local function sep_os_replacer(str)
 end
 local join_path = require("lvim.utils").join_paths
 
+M.python = function()
+    local conda_python = os.getenv("CONDA_PREFIX") .. "/bin/python"
+    require("dap-python").setup(conda_python)
+
+    dap.configurations.python = dap.configurations.python or {}
+    table.insert(dap.configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "launch with options",
+        program = "${file}",
+        python = function() end,
+        pythonPath = function()
+            local path
+            for _, server in pairs(vim.lsp.buf_get_clients()) do
+                if server.name == "pyright" or server.name == "pylance" then
+                    path = vim.tbl_get(server, "config", "settings", "python", "pythonPath")
+                    break
+                end
+            end
+            path = vim.fn.input("Python path: ", path or "", "file")
+            return path ~= "" and vim.fn.expand(path) or nil
+        end,
+
+        args = function()
+            local args = {}
+            local i = 1
+            while true do
+                local arg = vim.fn.input("Argument [" .. i .. "]: ")
+                if arg == "" then
+                    break
+                end
+                args[i] = arg
+                i = i + 1
+            end
+            return args
+        end,
+        justMyCode = function()
+            local yn = vim.fn.input("justMyCode? [y/n]: ")
+            if yn == "y" then
+                return true
+            end
+            return false
+        end,
+        stopOnEntry = function()
+            local yn = vim.fn.input("stopOnEntry? [y/n]: ")
+            if yn == "y" then
+                return true
+            end
+            return false
+        end,
+        console = "integratedTerminal",
+    })
+end
+
 function M.lua()
     dap.configurations.lua = {
         {
@@ -76,6 +130,7 @@ end
 
 function M.config()
     M.lua()
+    M.python()
     M.cpp()
 end
 
--- dot_config/lvim/lua/user/lsp/languages/python.lua
+++ dot_config/lvim/lua/user/lsp/languages/python.lua
@@ -5,19 +5,13 @@ formatters.setup({
 })
 
 -- Set a linter.
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({
-    {
-        command = "ruff",
-        filetypes = { "python" },
-        -- extra_args = { "--fix", "-e", "-n", "--stdin-filename", "$FILENAME", "-" },
-    },
-})
-
--- local util = require("lspconfig/util")
--- local path = util.path
-
--- vim.g.python3_host_prog = vim.env.CONDA_PREFIX .. "/bin/python"
+-- local linters = require("lvim.lsp.null-ls.linters")
+-- linters.setup({
+--     {
+--         command = "ruff",
+--         filetypes = { "python" },
+--     },
+-- })
 
 -- set lsp
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright", "ruff_lsp" })
@@ -26,21 +20,42 @@ local lsp_manager = require("lvim.lsp.manager")
 lsp_manager.setup("pyright", {
     on_init = require("lvim.lsp").common_on_init,
     capabilities = require("lvim.lsp").common_capabilities(),
+    settings = {
+        pyright = {
+            disableLanguageServices = false,
+            disableOrganizeImports = false,
+        },
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true,
+            },
+        },
+    },
+    single_file_support = true,
+    filetypes = { "python" },
 })
 
--- Setup dap for python
-local conda_python = os.getenv("CONDA_PREFIX") .. "/bin/python"
-pcall(function()
-    require("dap-python").setup(conda_python)
-    -- require("dap-python").setup("python")
-end)
+local ruff_opts = {
+    single_file_support = true,
+    filetypes = { "python" },
+}
 
--- Supported test frameworks are unittest, pytest and django. By default it
--- tries to detect the runner by probing for pytest.ini and manage.py, if
--- neither are present it defaults to unittest.
-pcall(function()
-    require("dap-python").test_runner = "pytest"
-end)
+lsp_manager.setup("ruff_lsp", ruff_opts)
+
+-- -- Setup dap for python
+-- local conda_python = os.getenv("CONDA_PREFIX") .. "/bin/python"
+-- pcall(function()
+--     require("dap-python").setup(conda_python)
+-- end)
+
+-- -- Supported test frameworks are unittest, pytest and django. By default it
+-- -- tries to detect the runner by probing for pytest.ini and manage.py, if
+-- -- neither are present it defaults to unittest.
+-- pcall(function()
+--     require("dap-python").test_runner = "pytest"
+-- end)
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "python" },
