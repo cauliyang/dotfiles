bd7f9c98d5906dd6a51e2da5521e012924d02b14
Yangyang Li <yangyang.li@northwestern.edu>
2023-07-16 18:44:39 -0500

    Update .config/zellij/plugins_repo/.git/config Update .config/zellij/plugins_repo/.git/index Add .config/zellij/plugins_repo/.git/modules/jbz/FETCH_HEAD Add .config/zellij/plugins_repo/.git/modules/jbz/HEAD Add .config/zellij/plugins_repo/.git/modules/jbz/config Add .config/zellij/plugins_repo/.git/modules/jbz/description Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/applypatch-msg.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/commit-msg.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/fsmonitor-watchman.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/post-update.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/pre-applypatch.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/pre-commit.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/pre-merge-commit.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/pre-push.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/pre-rebase.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/pre-receive.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/prepare-commit-msg.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/push-to-checkout.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/sendemail-validate.sample Add .config/zellij/plugins_repo/.git/modules/jbz/hooks/update.sample Add .config/zellij/plugins_repo/.git/modules/jbz/index Add .config/zellij/plugins_repo/.git/modules/jbz/info/exclude Add .config/zellij/plugins_repo/.git/modules/jbz/logs/HEAD Add .config/zellij/plugins_repo/.git/modules/jbz/logs/refs/heads/main Add .config/zellij/plugins_repo/.git/modules/jbz/logs/refs/remotes/origin/HEAD Add .config/zellij/plugins_repo/.git/modules/jbz/objects/info/.keep Add .config/zellij/plugins_repo/.git/modules/jbz/objects/pack/pack-0edc7031c9878a47d1d600a0a4f7bff2975671f7.idx Add .config/zellij/plugins_repo/.git/modules/jbz/objects/pack/pack-0edc7031c9878a47d1d600a0a4f7bff2975671f7.pack Add .config/zellij/plugins_repo/.git/modules/jbz/objects/pack/pack-0edc7031c9878a47d1d600a0a4f7bff2975671f7.rev Add .config/zellij/plugins_repo/.git/modules/jbz/packed-refs Add .config/zellij/plugins_repo/.git/modules/jbz/refs/heads/main Add .config/zellij/plugins_repo/.git/modules/jbz/refs/remotes/origin/HEAD Add .config/zellij/plugins_repo/.git/modules/jbz/refs/tags/.keep Add .config/zellij/plugins_repo/.git/objects/3d/6bac34e08c2bfad1af94c93a0c8ca8d51d19d7 Update .config/zellij/plugins_repo/.gitmodules Add .config/zellij/plugins_repo/jbz/Cargo.lock Add .config/zellij/plugins_repo/jbz/Cargo.toml Add .config/zellij/plugins_repo/jbz/LICENSE Add .config/zellij/plugins_repo/jbz/README.md Add .config/zellij/plugins_repo/jbz/bacon.toml Add .config/zellij/plugins_repo/jbz/.cargo/config.toml Add .config/zellij/plugins_repo/jbz/.git Add .config/zellij/plugins_repo/jbz/.gitignore Add .config/zellij/plugins_repo/jbz/.justfile Add .config/zellij/plugins_repo/jbz/.pre-commit-config.yaml Add .config/zellij/plugins_repo/jbz/jbz.cast Add .config/zellij/plugins_repo/jbz/jbz.gif Add .config/zellij/plugins_repo/jbz/src/main.rs Update .config/zellij/plugins_repo/justfile

---
+++ dot_config/zellij/plugins_repo/jbz/README.md
@@ -0,0 +1,11 @@
+# Just Bacon Zellij
+
+A [Zellij](https://zellij.dev/) plugin to display your [just](https://github.com/casey/just) commands wrapped in
+[bacon](https://github.com/Canop/bacon).
+
+Try it in a `zellij` session after a `cargo build`:
+```
+zellij action start-or-reload-plugin file:target/wasm32-wasi/debug/jbz.wasm
+```
+
+![demo](./jbz.gif)
---
+++ dot_config/zellij/plugins_repo/jbz/src/main.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+use regex::Regex;
+use zellij_tile::prelude::*;
+
+#[derive(Default)]
+struct State {
+    build_runs: usize,
+    commands: Vec<String>,
+}
+
+register_plugin!(State);
+
+fn just_commands() -> Vec<String> {
+    // let output = Command::new("just").arg("-l").output().unwrap();
+    // ↑ won't work in wasi, let's find another way
+    let file = File::open("/host/.justfile").unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+    // regex is another way, which kinda work here, but might not be optimal
+    // ref. https://github.com/casey/just/issues/365#issuecomment-1610357375
+    Regex::new(r"\n.*:\n")
+        .unwrap()
+        .find_iter(&contents)
+        .filter_map(|cmd| cmd.as_str().trim().strip_suffix(':'))
+        .map(|cmd| cmd.to_string())
+        .collect()
+}
+
+impl ZellijPlugin for State {
+    fn load(&mut self) {
+        hide_self();
+        subscribe(&[EventType::Key]);
+        self.commands = just_commands();
+        for cmd in &self.commands {
+            open_command_pane("bacon", vec!["just", "--", cmd]);
+        }
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        let should_render = false;
+        if let Event::Key(Key::Char('b')) = event {
+            self.build_runs += 1;
+            open_command_pane_floating("cargo", vec!["build"]);
+        }
+        should_render
+    }
+
+    fn render(&mut self, rows: usize, cols: usize) {
+        println!();
+        println!("I have {rows} rows and {cols} columns");
+        println!();
+        println!("here are the available commands:");
+        for cmd in &self.commands {
+            println!(" - {cmd}");
+        }
+        println!();
+        if self.build_runs > 0 {
+            println!("Ran tests {} times!", self.build_runs);
+        }
+    }
+}
