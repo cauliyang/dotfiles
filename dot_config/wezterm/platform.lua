c6bd87124c1a9cb70e6c2706a27f1a0e504e5b2c
Yangyang Li <yangyang.li@northwestern.edu>
2023-10-04 09:48:43 -0500

    Update .config/wezterm/platform.lua

--- dot_config/wezterm/platform.lua
+++ dot_config/wezterm/platform.lua
@@ -21,8 +21,8 @@ if utils.is_windows() then
 	}
 elseif utils.is_mac() then
 	M.config = {
-		window_background_opacity = 0.85,
-		macos_window_background_blur = 20,
+		window_background_opacity = 0.6,
+		macos_window_background_blur = 15,
 		font = utils.font_with_fallback({
 			family = "Cascadia Code",
 			harfbuzz_features = {
