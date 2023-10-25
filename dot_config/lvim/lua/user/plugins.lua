bdef7ef138b6143966a75b5e2fa44a44bd90c5df
Yangyang Li <yangyang.li@northwestern.edu>
2023-10-25 00:54:53 -0500

    Update .config/lvim/lazy-lock.json Update .config/lvim/lua/user/plugins.lua

--- dot_config/lvim/lua/user/plugins.lua
+++ dot_config/lvim/lua/user/plugins.lua
@@ -198,7 +198,7 @@ lvim.plugins = {
     },
 
     -- latex and markdown
-    -- https://github.com/iamcco/markdown-preview.nvim/tree/
+    -- https://github.com/iamcco/markdown-preview.nvim/
     {
         "iamcco/markdown-preview.nvim",
         build = "cd app && npm install",
