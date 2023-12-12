64f8b34e7057a93065b7cb12c8e2635fae17e9c0
Yangyang Li <yangyang.li@northwestern.edu>
2023-12-12 10:47:26 -0600

    Update .config/lvim/lazy-lock.json Update .config/lvim/lua/user/plugins.lua

--- dot_config/lvim/lua/user/plugins.lua
+++ dot_config/lvim/lua/user/plugins.lua
@@ -916,4 +916,27 @@ lvim.plugins = {
         end,
         enabled = lvim.builtin.symbol_usage.active,
     },
+
+    -- Preview code actions
+    {
+        "aznhe21/actions-preview.nvim",
+        config = function()
+            require("actions-preview").setup({
+                telescope = {
+                    sorting_strategy = "ascending",
+                    layout_strategy = "vertical",
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.9,
+                        prompt_position = "top",
+                        preview_cutoff = 20,
+                        preview_height = function(_, _, max_lines)
+                            return max_lines - 20
+                        end,
+                    },
+                },
+            })
+            vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+        end,
+    },
 }
