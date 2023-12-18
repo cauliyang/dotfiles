c8234585d84d3c1fe7fb6130d7547493b7bfff63
Yangyang Li <yangyang.li@northwestern.edu>
2023-12-18 13:29:58 -0600

    Update .config/lvim/lazy-lock.json Update .config/lvim/lua/user/lsp/init.lua

--- dot_config/lvim/lua/user/lsp/init.lua
+++ dot_config/lvim/lua/user/lsp/init.lua
@@ -13,11 +13,60 @@ require("user.lsp.languages.dockerfile")
 require("user.lsp.languages.typst")
 require("user.lsp.languages.javascript")
 
-vim.diagnostic.config({
+local icons = require("user.icons").icons
+
+local default_diagnostic_config = {
+    signs = {
+        active = true,
+        text = {
+            [vim.diagnostic.severity.ERROR] = icons.error,
+            [vim.diagnostic.severity.WARN] = icons.warn,
+            [vim.diagnostic.severity.INFO] = icons.info,
+            [vim.diagnostic.severity.HINT] = icons.hint,
+        },
+        values = {
+            { name = "DiagnosticSignError", text = icons.error },
+            { name = "DiagnosticSignWarn", text = icons.warn },
+            { name = "DiagnosticSignInfo", text = icons.info },
+            { name = "DiagnosticSignHint", text = icons.hint },
+        },
+    },
     virtual_text = false,
-})
+    underline = true,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        source = "if_many",
+        header = "",
+        prefix = "",
+        border = {
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+            { " ", "FloatBorder" },
+        },
+    },
+}
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
+
+lvim.lsp.document_highlight = true
+lvim.lsp.code_lens_refresh = true
+
+-- LSP lines
+vim.diagnostic.config({ virtual_lines = false })
+
+-- Setup diagnostics
+if lvim.builtin.lsplines then
+    default_diagnostic_config.virtual_text = false
+end
+
+vim.diagnostic.config(default_diagnostic_config)
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
