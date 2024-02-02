02090c4c1e6e77ab0d912445bb93c2c1c23cf198
Yangyang Li <yangyang.li@northwestern.edu>
2024-02-02 09:43:29 -0600

    Update .config/wezterm/platform.lua

--- dot_config/wezterm/platform.lua
+++ dot_config/wezterm/platform.lua
@@ -24,9 +24,9 @@ elseif utils.is_mac() then
 		window_background_opacity = 0.95,
 		macos_window_background_blur = 10,
 		font = utils.font_with_fallback({
-			-- family = "Cascadia Code",
+			family = "Cascadia Code",
 			-- https://github.com/githubnext/monaspace?tab=readme-ov-file
-			family = "Monaspace Argon",
+			-- family = "Monaspace Argon",
 			harfbuzz_features = {
 				"calt",
 				"liga",
@@ -43,34 +43,34 @@ elseif utils.is_mac() then
 				"dlig",
 			},
 		}),
-		-- font_rules = {
-		-- 	{
-		-- 		intensity = "Bold",
-		-- 		italic = true,
-		-- 		font = wezterm.font({
-		-- 			family = "Victor Mono",
-		-- 			weight = "Bold",
-		-- 			style = "Italic",
-		-- 		}),
-		-- 	},
-		-- 	{
-		-- 		italic = true,
-		-- 		intensity = "Half",
-		-- 		font = wezterm.font({
-		-- 			family = "Victor Mono",
-		-- 			weight = "DemiBold",
-		-- 			style = "Italic",
-		-- 		}),
-		-- 	},
-		-- 	{
-		-- 		italic = true,
-		-- 		intensity = "Normal",
-		-- 		font = wezterm.font({
-		-- 			family = "Victor Mono",
-		-- 			style = "Italic",
-		-- 		}),
-		-- 	},
-		-- },
+		font_rules = {
+			{
+				intensity = "Bold",
+				italic = true,
+				font = wezterm.font({
+					family = "Victor Mono",
+					weight = "Bold",
+					style = "Italic",
+				}),
+			},
+			{
+				italic = true,
+				intensity = "Half",
+				font = wezterm.font({
+					family = "Victor Mono",
+					weight = "DemiBold",
+					style = "Italic",
+				}),
+			},
+			{
+				italic = true,
+				intensity = "Normal",
+				font = wezterm.font({
+					family = "Victor Mono",
+					style = "Italic",
+				}),
+			},
+		},
 	}
 else
 	M.config = {
