0b81db7a7f8d4c8d46349c6b47f378321d4cc01c
Yangyang Li <yangyang.li@northwestern.edu>
2024-04-18 12:45:14 -0500

    Update .config/lvim/lazy-lock.json Update .config/lvim/lua/user/refactoring.lua Update .config/lvim/lua/user/theme.lua

--- dot_config/lvim/lua/user/refactoring.lua
+++ dot_config/lvim/lua/user/refactoring.lua
@@ -1,27 +1,30 @@
 local M = {}
 
 M.config = function()
-	local status_ok, refactoring = pcall(require, "refactoring")
-	if not status_ok then
-		return
-	end
+    local status_ok, refactoring = pcall(require, "refactoring")
+    if not status_ok then
+        return
+    end
 
-	refactoring.setup({
-		-- prompt for return type
-		prompt_func_return_type = {
-			go = true,
-			cpp = true,
-			c = true,
-			java = true,
-		},
-		-- prompt for function parameters
-		prompt_func_param_type = {
-			go = true,
-			cpp = true,
-			c = true,
-			java = true,
-		},
-	})
+    refactoring.setup({
+        -- prompt for return type
+        prompt_func_return_type = {
+            go = true,
+            cpp = true,
+            c = true,
+            java = true,
+            rust = true,
+        },
+        -- prompt for function parameters
+        prompt_func_param_type = {
+            go = true,
+            cpp = true,
+            c = true,
+            java = true,
+            python = true,
+            rust = true,
+        },
+    })
 end
 
 return M
