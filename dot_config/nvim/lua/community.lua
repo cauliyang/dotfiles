b14d52d0f57c1d09ca3653a8306e246ac0d137a1
Yangyang Li <yangyang.li@northwestern.edu>
2024-04-26 11:55:12 -0500

    Add .config/nvim/README.md Add .config/nvim/.neoconf.json Add .config/nvim/.stylua.toml Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/community.lua Add .config/nvim/lua/lazy_setup.lua Add .config/nvim/lua/plugins/astrocore.lua Add .config/nvim/lua/plugins/astrolsp.lua Add .config/nvim/lua/plugins/astroui.lua Add .config/nvim/lua/plugins/mason.lua Add .config/nvim/lua/plugins/nightfox.lua Add .config/nvim/lua/plugins/none-ls.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/user.lua Add .config/nvim/lua/polish.lua Add .config/nvim/neovim.yml Add .config/nvim/selene.toml

---
+++ dot_config/nvim/lua/community.lua
@@ -0,0 +1,30 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- example of importing an entire language pack
+  -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
+
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.color.transparent-nvim" },
+
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+}
